c72123258486f1bf53a3e2c97882b5a790fda80d
RingiBoy <mitya.info@gmail.com>
2022-01-13 22:21:47 +0200

    start master

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
-import './App.css';
-import Comments from './components/comments/Comments';
-import Posts from './components/posts/Posts';
-import Users from './components/users/Users';
+import React from 'react';
 
-function App() {
+const App = () => {
   return (
-    <div className='componentsCollumn'>
-      <div className='lineBox'> <div className='line'> <Users/></div><div > <Posts className='line'/></div></div>
-      <div className='lineC'><Comments/></div>
+    <div>
+      
     </div>
   );
-}
+};
 
 export default App;
-
 
 
